55fbbc35a6f9fcaf562db750850ffd37fa22e5b7
rhoerbe <rainer@hoerbe.at>
2021-03-31 08:33:14 +0200

    write samples without thermos.py

--- thermos.sh
+++ thermos.sh
@@ -3,13 +3,15 @@
 function write_temp() {
 
   for x in $(ls  /sys/bus/w1/devices/ | grep 28); do 
-    echo "$x ""$(cat /sys/bus/w1/devices/w1_bus_master1/${x}/temperature)"; 
-  done | python3 /home/r2h2/thermos.py >> /home/r2h2/logs/thermos/$fn
+    echo "$x ""$(cat /sys/bus/w1/devices/w1_bus_master1/${x}/temperature)"  >> /home/r2h2/logs/thermos/$dir/$fn
+  done
 
 }
 
 
 while true; do
+  dir=$(date --iso-8601)
+  midir $dir
   fn=$(date --iso-8601=minutes)
   write_temp
   sleep 300
