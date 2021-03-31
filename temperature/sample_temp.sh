7dd7b787faf54246b97657075b72dfc88ea21b60
rhoerbe <rainer@hoerbe.at>
2021-03-31 12:08:49 +0200

    fixes for sample temp and add systemd unit example

--- temperature/sample_temp.sh
+++ temperature/sample_temp.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/bash
 
-function write_temp() {
+logdirroot='/home/r2h2/logs/thermos'
+lastlogfp='/var/log/sample_temp/lastlog'
+sampling_interval=300
 
-  for x in $(ls  /sys/bus/w1/devices/ | grep 28); do 
-    echo "$x ""$(cat /sys/bus/w1/devices/w1_bus_master1/${x}/temperature)"  >> /home/r2h2/logs/thermos/$dir/$fn
+
+function main() {
+  while true; do
+    local todaydir=$(date --iso-8601)
+    mkdir -p $logdirroot/$todaydir
+    local fn=$(date --iso-8601=minutes)
+    fp=$logdirroot/$todaydir/$fn
+    write_temp
+    sleep $sampling_interval
   done
-
 }
 
 
-while true; do
-  dir=$(date --iso-8601)
-  midir $dir
-  fn=$(date --iso-8601=minutes)
-  write_temp
-  sleep 300
-done
+function write_temp() {
+
+  for x in $(ls  /sys/bus/w1/devices/ | grep 28); do 
+    echo "$x ""$(cat /sys/bus/w1/devices/w1_bus_master1/${x}/temperature)"  >> $fp
+  done
+  printf "$fp created with " >> $lastlogfp
+  stat --printf="%s" $fp  >> $lastlogfp
+  printf " bytes\n" >> $lastlogfp
+}
+
+
+main $@
