6e772055ba88d2e56527c31069144e224b590bf1
rhoerbe <rainer@hoerbe.at>
2021-04-03 23:19:27 +0200

    fix "pass logdir to updatedb"

--- upload2db/upload2db.sh
+++ upload2db/upload2db.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+logdir=$1
+
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 projdir=$(dirname $scriptdir)
 
 source venv/bin/activate
-python upload2db/upload2db.py
+python upload2db/upload2db.py $logdir
