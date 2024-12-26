0608788cd40bca1f22817587e7502b135679c05d
Rainer Hörbe <rhoerbe@users.noreply.github.com>
2024-12-26 17:49:01 +0100

    2. move redundant code to write_mqtt(), uid/pw from env

--- temperature/mqtt_pub.py
+++ temperature/mqtt_pub.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from pathlib import Path
 import paho.mqtt.client as mqtt
 
@@ -7,19 +8,16 @@ broker_addr = "10.4.4.17"
 broker_port = 1883
 sensor_root_path = Path('/sys/bus/w1/devices/w1_bus_master1/')
 
-sensors_heatpump = {
+sensors = {
     '28-0119354f4ef8': 't04-Holzofen_Rücklauf',
     '28-011935714abf': 't06-Boiler-oben',
     '28-01202251809c': 't07-Puffer-oben',
     '28-0120222c37ec': 't09-FBH-Vorlauf',
     '28-0119354de282': 't10-FBH-Rücklauf',
-}
-sensors_ventilation = {
     '28-3c01d6075254': 't01-Keller-Abluft',
     '28-01193555a909': 't05-EG-Abluft',
     '28-012022c38a09': 't08-OG-Abluft',
 }
-
 sensory_unused = {
     '28-012022bc4dd7': 't11',
     '28-01193558950f': 't12',
@@ -35,7 +33,7 @@ def main():
         temp_str = p.read_text().strip()
         temp = '{"temperature": ' + str(round(int(temp_str) / 1000, 1)) + '}'
         try:
-            topic = 'home/temperature/' + sensors_heatpump[sensor]
+            topic = 'home/temperature/' + sensors[sensor]
             if args.verbose:
                 print(topic + "   " + temp)
             client.publish(topic, temp)
@@ -46,11 +44,14 @@ def main():
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
     args = parser.parse_args()
     client = mqtt.Client(client_id="cloudberry")
-    client.username_pw_set("mqtt", "Crsm7.pvbwb9")
+    try:
+        password = os.environ['MQTT_PASSWORD']
+        userid = os.environ['MQTT_USERID']
+    except KeyError:
+        raise KeyError(f"Environment variable MQTT_PASSWORD or MQTT_USERID not found.")
+    client.username_pw_set(userid, password)
     client.connect(broker_addr, broker_port, 60)
-    for sensor in sensors_heatpump.keys():
-        write_mqtt()
-    for sensor in sensors_ventilation.keys():
+    for sensor in sensors.keys():
         write_mqtt()
 
 if __name__ == '__main__':
